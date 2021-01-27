c6f04ac61999b3a722e98e0512415ff4f86235e5
Valerii Mykhalchuk <Valerii.Mykhalchuk@halliburton.com>
2021-01-27 21:31:44 +0200

    Updated Logout in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,21 +14,9 @@ from utils.constants import DEFAULT_WAIT_TIME
 def browser():
     # driver = webdriver.Chrome(os.environ.get('CHROME_DRIVER_PATH'))
     driver = webdriver.Chrome('chromedriver.exe')  # put chromedriver folder path into PATH
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(DEFAULT_WAIT_TIME)
     driver.maximize_window()
     yield driver
-    current_url = driver.current_url
-    driver.get(current_url)
-    try:
-        wait = WebDriverWait(driver, DEFAULT_WAIT_TIME)
-        hello_user = wait.until(EC.visibility_of_element_located(SuitesPageLocators.HELLO_DPDN))
-        hello_user.click()
-        logout_click = wait.until(EC.visibility_of_element_located(SuitesPageLocators.LOGOUT_OPT))
-        logout_click.click()
-    except Exception as e:
-        print("error occurred", e)
-    finally:
-        pass
     driver.quit()
 
 
