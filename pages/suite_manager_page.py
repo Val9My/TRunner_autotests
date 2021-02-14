c24282428f671d45e3014611211bf064da0c6770
Kseniia <yahorlytska@gmail.com>
2021-02-14 19:44:54 +0200

    Added methods for selecting suites options from dropdown by index or by visible text.
    Added methods for getting test cases id and name.
    Added method for selecting checkbox

--- pages/suite_manager_page.py
+++ pages/suite_manager_page.py
@@ -1,3 +1,6 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import locators
@@ -19,3 +22,34 @@ class SuiteManagerPage(BasePageElement):
         wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
         trunner_lnk = wait.until(EC.visibility_of_element_located(locators.SuiteManagerPageLocators.TRUNNER_LNK))
         trunner_lnk.click()
+
+    def suites_option_select_by_index(self, n):
+        'Check that user is able to select suites from option selector, where n-number of option'
+        suite_selector_drp = Select(self.browser.find_element(*locators.SuiteManagerPageLocators.SUITE_SELECTOR_DPDN))
+        suite_selector_drp.select_by_index(n)
+        time.sleep(10)
+
+    def suites_option_select_by_visible_text(self, element_text):
+        'Check that user is able to select suites from option selector, where text-name of option'
+        suite_selector_drp = Select(self.browser.find_element(*locators.SuiteManagerPageLocators.SUITE_SELECTOR_DPDN))
+        suite_selector_drp.select_by_visible_text(element_text)
+        time.sleep(10)
+
+    def tc_checkbox_option_click(self, n):
+        'click on TC checkbox'
+        wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
+        tc_checkbox = wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ".test_case_row:nth-of-type(" + str(n) + ") .check-row")))
+        tc_checkbox.click()
+
+    def get_id_tc_value(self, n):
+        wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
+        id = wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ".table .test_case_row:nth-of-type(" + str(n) + ") .tcid")))
+        return int(id.text)
+
+    def get_tc_title(self, n):
+        wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
+        tc_title = wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ".test_case_row:nth-of-type(" + str(n) + ") td:last-child")))
+        return tc_title.text
