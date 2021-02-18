9fa896c41ddc71117b8f6f968aaeb88aa220baef
Kseniia <yahorlytska@gmail.com>
2021-02-18 21:04:05 +0200

    Added methods for modal window 'add test case to suite' 1)method for opening modal window 2)method that get titles of test cases 3)method for click add button 4)method that select checkbox options 5)method that close modal window

--- pages/suite_manager_page.py
+++ pages/suite_manager_page.py
@@ -24,32 +24,65 @@ class SuiteManagerPage(BasePageElement):
         trunner_lnk.click()
 
     def suites_option_select_by_index(self, n):
-        'Check that user is able to select suites from option selector, where n-number of option'
+        "Check that user is able to select suites from option selector, where n-number of option"
         suite_selector_drp = Select(self.browser.find_element(*locators.SuiteManagerPageLocators.SUITE_SELECTOR_DPDN))
         suite_selector_drp.select_by_index(n)
         time.sleep(10)
 
     def suites_option_select_by_visible_text(self, element_text):
-        'Check that user is able to select suites from option selector, where text-name of option'
+        "Check that user is able to select suites from option selector, where text-name of option"
         suite_selector_drp = Select(self.browser.find_element(*locators.SuiteManagerPageLocators.SUITE_SELECTOR_DPDN))
         suite_selector_drp.select_by_visible_text(element_text)
         time.sleep(10)
 
     def tc_checkbox_option_click(self, n):
-        'click on TC checkbox'
+        "Click on TC checkbox from dropdown"
         wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
         tc_checkbox = wait.until(EC.visibility_of_element_located(
             (By.CSS_SELECTOR, ".test_case_row:nth-of-type(" + str(n) + ") .check-row")))
         tc_checkbox.click()
 
     def get_id_tc_value(self, n):
+        "Get id of test case"
         wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
         id = wait.until(EC.visibility_of_element_located(
             (By.CSS_SELECTOR, ".table .test_case_row:nth-of-type(" + str(n) + ") .tcid")))
         return int(id.text)
 
     def get_tc_title(self, n):
+        "Get test case title"
         wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
         tc_title = wait.until(EC.visibility_of_element_located(
             (By.CSS_SELECTOR, ".test_case_row:nth-of-type(" + str(n) + ") td:last-child")))
         return tc_title.text
+
+    def add_modal_window(self):
+        "Opening modal 'Add to Suite' window by clicking on +Add button"
+        wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
+        add_lnk = wait.until(EC.visibility_of_element_located(locators.SuiteManagerPageLocators.ADD_CASE_TO_SUITE_BTN))
+        add_lnk.click()
+
+    def get_tc_title_in_modal_add_window(self):
+        "Get test case name in modal window 'Add to Suite'"
+        wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
+        test_case_name = wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, "tbody>tr>td:nth-of-type(" + str(n) + ")")))
+        return test_case_name.text
+
+    def click_add_button(self):
+        "Click on  button 'Add Test Cases' in modal window 'Add to Suite'"
+        add_button = self.browser.find_element(*locators.SuiteManagerPageLocators.ADD_TC_BUTTON_MODALW)
+        add_button.click()
+
+    def click_checkbox_in_modal_add_window(self):
+        "Select checkbox of test cases in 'Add Test Cases' modal window"
+        wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
+        new = wait.until(EC.element_to_be_clickable(locators.SuiteManagerPageLocators.CHECKBOX_TC_MODALW))
+        new.click()
+
+    def close_add_test_modal_window(self):
+        "Close 'Add to Suite' modal window"
+        wait = WebDriverWait(self.browser, DEFAULT_WAIT_TIME)
+        close_button = wait.until(
+            EC.element_to_be_clickable(locators.SuiteManagerPageLocators.CLOSE_ADD_TEST_CASE_MODALW))
+        close_button.click()
